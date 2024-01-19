a4a9303ccb3f33b7e8d0ed804766414286e3073d
Alexander Martins <alexandermartins123@icloud.com>
2024-01-19 12:22:39 +0100

    made some changes

--- index.js
+++ index.js
@@ -18,19 +18,18 @@ const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-// const corsOptions = {
-//   origin: "http://localhost:3000",
-//   credentials: true,
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-
-// };
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
 
 app.use(cors());
 
 // app.options("/api/admin/logout", cors(corsOptions));
-app.post("/api/admin/logout", cors(), logoutUser);
-app.post("/api/contractor/logout", cors(), logoutUser);
-app.post("/api/stakeholder/logout", cors(), logoutUser);
+app.post("/api/admin/logout", cors(corsOptions), logoutUser);
+app.post("/api/contractor/logout", cors(corsOptions), logoutUser);
+app.post("/api/stakeholder/logout", cors(corsOptions), logoutUser);
 
 app.post("/api/admin/login", cors(), authUser);
 app.post("/api/contractor/login", cors(), loginContractor);
