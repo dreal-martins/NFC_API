ced52882a4a72d2195760ce1fee64e7eb668068e
Alexander Martins <alexandermartins123@icloud.com>
2024-01-19 13:17:00 +0100

    made some changes

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/api/admin/login", cors(), authUser);
 app.post("/api/contractor/login", cors(), loginContractor);
 app.post("/api/stakeholder/login", cors(), loginStakeholder);
 
-app.post("/api/admin/logout", cors(), logoutUser);
+app.post("/api/admin/logout", cors(corsOptions), logoutUser);
 app.post("/api/contractor/logout", cors(), logoutUser);
 app.post("/api/stakeholder/logout", cors(), logoutUser);
 
